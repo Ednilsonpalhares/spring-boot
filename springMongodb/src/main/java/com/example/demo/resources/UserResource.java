7f985d1982e59a323aad35c87aafc209da45979a
ednilson <ednilsonpalhares@hotmail.com>
2019-11-06 04:27:26 -0300

    projeto Criado

--- springMongodb/src/main/java/com/example/demo/resources/UserResource.java
+++ springMongodb/src/main/java/com/example/demo/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
@@ -20,7 +21,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@GetMapping
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
