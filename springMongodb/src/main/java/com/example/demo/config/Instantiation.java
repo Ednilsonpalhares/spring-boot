23609984e6f9d7887b9c5c3af49e6bd8a85c65ea
ednilson <ednilsonpalhares@hotmail.com>
2019-11-09 23:38:25 -0300

    referenciando posts do usuario

--- springMongodb/src/main/java/com/example/demo/config/Instantiation.java
+++ springMongodb/src/main/java/com/example/demo/config/Instantiation.java
@@ -1,22 +1,33 @@
 package com.example.demo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.dominio.Post;
 import com.example.demo.dominio.Usuario;
+import com.example.demo.dto.AuthorDTO;
+import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UsuarioRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
+	private PostRepository postRepository;
+	
+	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
 	@Override
 	public void run(String...arg0) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		usuarioRepository.deleteAll();
 		
@@ -25,5 +36,13 @@ public class Instantiation implements CommandLineRunner{
 		Usuario bob = new Usuario(null, "Bob Grey", "bob@gmail.com");
 		
 		usuarioRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		usuarioRepository.save(maria);
 	}
 }
--- springMongodb/src/main/java/com/example/demo/dominio/Post.java
+++ springMongodb/src/main/java/com/example/demo/dominio/Post.java
@@ -1,32 +1,33 @@
 package com.example.demo.dominio;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("usuario")
-public class Usuario implements Serializable {
+import com.example.demo.dto.AuthorDTO;
+
+@Document
+public class Post implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	private String id;
-	private String name;
-	private String email;
+	private Date date;
+	private String title;
+	private String body;
+	private AuthorDTO author;
 	
-	@DBRef(lazy = true)
-	private List<Post> posts = new ArrayList<>();
+	public Post(){}
 	
-	public Usuario() {}
-	
-	public Usuario(String id, String name, String email) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		this.id = id;
-		this.name = name;
-		this.email = email;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.author = author;
 	}
 
 	public String getId() {
@@ -37,28 +38,36 @@ public class Usuario implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public Date getDate() {
+		return date;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setDate(Date date) {
+		this.date = date;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public List<Post> getPosts() {
-		return posts;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
-	public void setPosts(List<Post> posts) {
-		this.posts = posts;
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
 	}
 
 	@Override
@@ -77,7 +86,7 @@ public class Usuario implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Usuario other = (Usuario) obj;
+		Post other = (Post) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -85,5 +94,5 @@ public class Usuario implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
